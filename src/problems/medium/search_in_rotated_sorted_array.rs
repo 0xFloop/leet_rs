b0ac90684cf8c9beff7dd02c56b9962b655e77a1
0xFloop <95703085+0xFloop@users.noreply.github.com>
2024-09-11 20:57:50 -0600

    fixxy wixxy

--- src/problems/medium/search_in_rotated_sorted_array.rs
+++ src/problems/medium/search_in_rotated_sorted_array.rs
@@ -1,33 +1,34 @@
 pub fn solve() -> i32 {
-    let nums = vec![4, 5, 6, 7, 0, 1, 2];
+    let nums = vec![5, 1, 3];
     let target = 3;
-    //this does not solve the problem in the requested O(logn) time complexity
-    match nums.iter().position(|&r| r == target) {
-        Some(v) => return v as i32,
-        None => return -1,
-    }
 
-    //this is my attempt to solve with the requestd complexity it using binary search but
-    //the whole out of order thing is really confusing my brain
-    //
-    // let mut lo = 0;
-    // let mut hi = nums.len() - 1;
-    //
-    // while lo <= hi {
-    //     let mid = (lo + hi) / 2;
-    //
-    //     if nums[mid] == target {
-    //         return mid as i32;
-    //     }
-    //
-    //     if nums[lo] <= nums[mid] {
-    //         if nums[lo] <= target && nums[hi] > target {
-    //             hi = mid - 1;
-    //         } else {
-    //             lo = mid + 1;
-    //         }
-    //     } else {
-    //     }
-    // }
-    // -1
+    let mut lo = 0;
+    let mut hi = nums.len() - 1;
+
+    while lo <= hi {
+        let mid = (lo + hi) / 2;
+
+        let l = nums[lo];
+        let r = nums[hi];
+        let m = nums[mid];
+
+        if m == target {
+            return mid as i32;
+        }
+
+        if l <= m {
+            if l <= target && m >= target {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        } else {
+            if m <= target && r >= target {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+    }
+    -1
 }
